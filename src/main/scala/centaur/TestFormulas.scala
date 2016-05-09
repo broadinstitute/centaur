9330bbaab49953a1ec9cabb92ffd0549f8288b9f
Ruchi Munshi <rmunshi@broadinstitute.org>
2016-05-09 17:09:56 -0400

    removed unused caching method

--- src/main/scala/centaur/TestFormulas.scala
+++ src/main/scala/centaur/TestFormulas.scala
@@ -25,13 +25,6 @@ object TestFormulas {
     } yield ()
   }
 
-  def runCachingWorkflow(request: WorkflowRequest) = {
-    for {
-      s <- runWorkflowUntilTerminalStatus(request, Succeeded)
-      _ <- verifyCaching(s, request)
-    } yield ()
-  }
-
   def runFailingWorkflow(request: WorkflowRequest) = runWorkflowUntilTerminalStatus(request, Failed)
 
   def runSubmissionFailureWorkflow(request: WorkflowRequest) = submitWorkflowExpectingRejection(request)
