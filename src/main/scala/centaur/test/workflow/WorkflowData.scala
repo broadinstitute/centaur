9664b0f483da0f1057839ba55bbaff54a8f37b80
Ruchi Munshi <rmunshi@broadinstitute.org>
2016-07-19 10:06:43 -0400

    centaur not adding in credentials to options properly. Confirming test_cromwell passes in token.

--- src/main/scala/centaur/test/workflow/WorkflowData.scala
+++ src/main/scala/centaur/test/workflow/WorkflowData.scala
@@ -29,11 +29,8 @@ case class WorkflowOptions(options: Option[String]) {
     options match {
       case Some(someOptions) =>
         val optionsMap = someOptions.parseJson.asJsObject.convertTo[Map[String, JsValue]]
-        val gotIt = WorkflowOptions(Some(addToken(optionsMap).toJson.toString))
-        println(s"the new options are: $gotIt")
-        gotIt
+        WorkflowOptions(Some(addToken(optionsMap).toJson.toString))
       case None =>
-        println(s"No options")
         WorkflowOptions(options)
     }
   }
